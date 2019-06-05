f47d3db3b9537c43fb4148022c0da093b90a8cc9
Katherine Kerr <katherinekerr@protonmail.com>
2019-06-05 13:45:58 +0100

    adds start for lab

---
+++ pizza_controller.rb
@@ -0,0 +1,32 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative('./models/pizza_order')
+also_reload('./models/*')
+
+# order to write things in:
+# db -> models -> controller -> views
+
+#INDEX
+get '/pizza-orders' do
+  @orders = PizzaOrder.all()
+  erb(:index)
+end
+
+#NEW
+get '/pizza-orders/new' do
+  erb(:new)
+end
+
+#CREATE
+post '/pizza-orders' do
+  @order = PizzaOrder.new(params)
+  @order.save
+  erb(:create)
+end
+
+#SHOW
+get '/pizza-orders/:id' do
+  @order = PizzaOrder.find(params[:id].to_i) #params hash understands both symbols and strings, so could put "id" instead of :id
+  erb(:show)
+end
